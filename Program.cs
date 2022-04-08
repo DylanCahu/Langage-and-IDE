3a89b0d8b4c4ce1f7c72c448a41d903da0036bd3
DylanCahu <dydypot@gmail.com>
2022-04-08 11:22:34 +0200

    Saving Test

--- Program.cs
+++ Program.cs
@@ -1,27 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.IO;
 
-namespace CodeFicherGraphique
+namespace readfile
 {
-    static class Program
+    class Test
     {
-
-
-        static public Variables mesVariables;//nos variables utilisé dans le programme
-        static public Form1 form1;
-        [STAThread]
-        static void Main()
+        static string [] tabline ;
+        static int cpt=0;
+        static void lire()
         {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("P:\\SIO2\\Slam\\C#\\c-sharp\\readfile\\text.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    cpt++;
+                    //write the line to console window
+                    Console.WriteLine(cpt+" : "+ line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                    
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine("Exception: " + f.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
 
+        static void ecrire()
+        {
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter("P:\\SIO2\\Slam\\C#\\c-sharp\\readfile\\text.txt");
+                //Write a line of text
+                sw.WriteLine("Hello World!!");
+                //Write a second line of text
+                sw.WriteLine("From the StreamWriter class");
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
 
-            mesVariables = new Variables();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            form1 = new Form1();
-            Application.Run(form1);
+        public static void Main()
+        {
+            lire();
         }
     }
 }
+
